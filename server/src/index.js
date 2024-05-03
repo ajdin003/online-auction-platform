1ba4acb060fc0dd362bf4946be3c6a0299e05f6b
Ajdin <ahusetovic1@gmail.com>
2024-05-03 12:36:05 +0200

    Change to mo mongodb, setup database

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('Online Auction Platform Server');
+app.use("/", (req, res) => {
+  res.send("Online Auction Platform Server");
+});
+
+const DB = process.env.DATABASE_CONNECTION.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log("Database connection established.");
 });
 
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
