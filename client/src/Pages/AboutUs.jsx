4b16af8941b77ee51ab1f27375d963b366ddee36
HamzaaaC <hamza.celikovic.22@size.ba>
2024-05-28 23:05:57 +0200

    oppp

---
+++ client/src/Pages/AboutUs.jsx
@@ -0,0 +1,35 @@
+/*import React from "react";*/
+import React, { useRef } from 'react';
+import "../css/about.css";
+
+const AboutUs = () => {
+
+    return (
+        <div class="sve">
+        <div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container bottomContainer">
+    <div class="allText bottomText">
+      <p class="text-blk headingText">
+        About Us
+      </p>
+      <p class="text-blk subHeadingText">
+        Lorem ipsum dolor sit amet.
+      </p>
+      <p class="text-blk description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
+      </p>
+      <p class="text-blk description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum pulvinar ullamcorper suspendisse ac eget. Pellentesque tempus leo in ullamcorper quis vestibulum ligula elementum ut. Congue in dignissim tincidunt ut dolor eu. Mi, eget posuere vitae sed purus nisl lorem.
+      </p>
+    </div>
+    <div class="videoContainer">
+      <iframe allowfullscreen="allowfullscreen" class="mainVideo" controls="controls" src="https://www.youtube.com/embed/svg%3E?">
+      </iframe>
+      
+    </div>
+  </div>
+</div>
+</div>   );
+};
+
+export default AboutUs;
